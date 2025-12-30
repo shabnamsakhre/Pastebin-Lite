cb8f431cef4ae01f4cacc666a06a84acad63942c
shabnamsakhre <sakhreshabnam@gmail.com>
2025-12-30 20:22:19 +0530

    Add backend URL to frontend, ready to deploy frontend.

--- frontend/src/config/axios.jsx
+++ frontend/src/config/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://pastebin-lite-liart.vercel.app/",
 });
 
 export default instance;
